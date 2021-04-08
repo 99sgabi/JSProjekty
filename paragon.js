5a428045eaf3bfaf7ffde7f857c9587b6c8d6c3f
99sgabi <99sgabi@wp.pl>
2021-04-08 20:26:01 +0200

    walidacja + wydzielenie do funckji

--- paragon.js
+++ paragon.js
@@ -1,21 +1,17 @@
-// G -> trzeba zrobic funkcje do walidacji - sprawdzanie czy nie jest pusta + czy nie sa wartosci ujemne
+// + G -> trzeba zrobic funkcje do walidacji - sprawdzanie czy nie jest pusta + czy nie sa wartosci ujemne
 // E -> dodawnie elementów do tabeli
 // G -> edycja istniejacych  - sumowanie wszystkich wierszy i zamiana na input; button
 // K -> usuwanie elementów - suma jeszcze raz puścić przy zmianie; button
 // K -> zmiana kolejności (próba drag & drop), ewentualnie zamiana wierszy 
 // E -> css
 let receipt = null;
+const form = document.getElementById("addingNewElement");
 let table = document.getElementsByTagName("table")[0];
 
 class Item{
     constructor(name, count, price)
     {
         this.update(name,count,price)
-    }
-
-    sum()
-    {
-        return (this.count)*(this.price);
     }
 
     update(name, count, price)
@@ -26,23 +22,53 @@ class Item{
     }
 }
 
+function validateItemData(name, count, price)
+{
+    if(name == null || name == "")
+    {
+        alert("Nalezy podac nazwe produktu, nie moze byc pusta!")
+        return false;
+    }
+        
+    if(count <= 0 || price <= 0)
+    {
+        alert("Cena, jak i ilosc musza byc liczbami dodatnimi")
+        return false;
+    }
+     
+    return true;
+}
+
+function sumTableElements()
+{
+
+}
+
+function createNewRowInTable(itemLp, itemName, itemCount, itemPrice)
+{
+    console.log(table)
+    let newRow =table.insertRow(itemLp);
+    let lp = newRow.insertCell();
+    lp.innerHTML = itemLp;
+    let name = newRow.insertCell();
+    name.innerHTML = itemName;
+    let amout = newRow.insertCell()
+    amout.innerHTML = itemCount;
+    let price = newRow.insertCell()
+    price.innerHTML = itemPrice;
+    let sum = newRow.insertCell()
+    sum.innerHTML = itemCount * itemPrice;
+    let button = newRow.insertCell()
+    button.innerHTML = '<button name="edit">Edytuj</button> <br/> <button name="delete">Usun</button>';
+}
+
 function displayItems(itemList)
 {
     for(let i=0; i< itemList.length; i++)
     {
-        let newRow =table.insertRow(i + 1);
-        let lp = newRow.insertCell();
-        lp.innerHTML = i + 1;
-        let name = newRow.insertCell();
-        name.innerHTML = itemList[i].name;
-        let amout = newRow.insertCell()
-        amout.innerHTML = itemList[i].count;
-        let price = newRow.insertCell()
-        price.innerHTML = itemList[i].price;
-        let sum = newRow.insertCell()
-        sum.innerHTML = itemList[i].count * itemList[i].price;
+        createNewRowInTable(i+1, itemList[i].name, itemList[i].count, itemList[i].price)
     }
-    console.log(table.rows.length)
+    sumTableElements();
 }
 
 function clearTable()
@@ -67,8 +93,12 @@ window.onbeforeunload = function() {
     localStorage["receipt"] = JSON.stringify(receipt);
 }
 
-/*let form = document.getElementById("addingNewElement");
-form.onsubmit((event) => {
+form.onsubmit = (event) => {
+    //walidajca
+    validateItemData(
+        form.name.value, parseFloat(form.count.value), parseFloat(form.price.value))
+    
 
-})*/
-setTimeout( () => clearTable(), 1000);
+    event.preventDefault();
+}
+//setTimeout( () => clearTable(), 1000);
