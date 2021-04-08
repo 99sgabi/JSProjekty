83ea46034aee9b6dd8bbaa05db93bda6951abbcf
99sgabi <73700137+99sgabi@users.noreply.github.com>
2021-04-08 17:17:46 +0200

    Update paragon.js

--- paragon.js
+++ paragon.js
@@ -16,11 +16,32 @@ class Item{
         this.count = count;
     }
 }
-
+let itemList = [new Item("paczki", 10, 1.45), new Item("lukier", 12, 5.00)];
 let receipt = window.localStorage.getItem("receipt");
-let table = document.getElementsByTagName("table");
 if(receipt != null)
 {
-    window.localStorage.setItem("receipt", []);
+    window.localStorage.setItem("receipt", itemList);
     receipt = window.localStorage.getItem("receipt");
 }
+
+function displayItems(itemList)
+{
+    let table = document.getElementsByTagName("table")[0];console.log(table)
+    for(let i=0; i< itemList.length; i++)
+    {
+        let newRow =table.insertRow();
+        let lp = newRow.insertCell();
+        lp = i + 1;
+        let name = newRow.insertCell();
+        name = itemList[i].name;
+        let amout = newRow.insertCell()
+        amout = itemList[i].count;
+        let price = newRow.insertCell()
+        price = itemList[i].price;
+        let sum = newRow.insertCell()
+        sum = itemList[i].sum();
+    }
+}
+console.log(itemList)
+
+displayItems(itemList)
