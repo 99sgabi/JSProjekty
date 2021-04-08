67650da5fe2f438afc6dd6bc369e025d03f6087c
99sgabi <73700137+99sgabi@users.noreply.github.com>
2021-04-08 15:58:54 +0200

    Update paragon.js

--- paragon.js
+++ paragon.js
@@ -1,9 +1,18 @@
 class Item{
     constructor(name, count, price)
     {
+        this.update(name,count,price)
+    }
+
+    sum()
+    {
+        return this.count*this.sum;
+    }
+
+    update(name, count, price)
+    {
         this.price = price;
         this.name = name;
         this.count = count;
     }
-
 }
